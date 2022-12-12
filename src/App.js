d4089a3a4a0f7ccb6180c61e9151621c62a19e76
ausrinesa <a.sabockyte97@gmail.com>
2022-12-12 13:36:35 +0200

    reactApp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search/search.component';
 import './App.css';
 
 class App extends Component {
@@ -14,44 +15,53 @@ class App extends Component {
   }
 
   componentDidMount(){
-    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users)=>
       this.setState(
         ()=>{
           return {monsters:users};
-        },
-        ()=> {
-          console.log(this.state);
         }
        )
       );
   }
 
+  onSearchChange = (event)=>{
+    const searchField = event.target.value.toLocaleLowerCase();
+    
+    this.setState(() => {
+      return { searchField };
+    })
+  }
+////^^this is an optimisation, instead of having the function inside onChange in input, in this way the method does not need to be re-initalised several times
+
   render(){
 
-    const filteredMonsters = this.state.monsters.filter((monster)=>{
-             return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+//// ^ variables initialised, so code is made more readable
+
+    const filteredMonsters = monsters.filter((monster)=>{
+             return monster.name.toLocaleLowerCase().includes(searchField);
           });
 
     return (
      <div className="App">
+      <h1 className='title'>Monsters App React</h1>
+      <SearchBox 
+      onChangeHandler={onSearchChange} 
+      placeholder='search monsters' 
+      className='monsters-search-box' 
+      />
 
-      <input className='search-box' type='search' placeholder='search monsters' onChange={
-        (event)=>{
-          const searchField = event.target.value.toLocaleLowerCase();
-          
-          this.setState(() => {
-            return { searchField };
-          })
-        }} 
-        />
-
-      {filteredMonsters.map((monster)=>{
+      {/* {filteredMonsters.map((monster)=>{
           return <h1 key={monster.id}> {monster.name} </h1>
         })
-      }
+      } */}
+
+{/* must be capital cased so that it is clear its 'human made component' */}
+      <CardList monsters={filteredMonsters} />
+      
     </div>
     );  
   }
